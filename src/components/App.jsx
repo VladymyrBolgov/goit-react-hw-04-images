b237a8f62cd2637e56ad857e4b81253a9a1ce463
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-02 20:02:07 +0300

    srs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,111 +1,87 @@
-import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
-import { Container } from './Container.styled'
-import ContactForm from 'components/ContactForm';
-import Filter from 'components/Filter';
-import ContactList from 'components/ContactList';
+import React, { Component } from "react";
+import { Notify } from "notiflix";
+import { Global } from '@emotion/react'
+import { GlobalStyles } from "./GlobalStyles.styled";
+import { AppBox } from "./App.styled";
+
+import getImages from '../services/api'
+import Loader from "./Loader";
+import Button from "./Button";
+import Searchbar from "./Searchbar";
+import ImageGallery from "./ImageGallery";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    value: '',
+    images: [],
+    page: 1,
+    isLoading: false,
   };
-// При первой загрузке приложения - стадия Монтирования
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
 
-    if (parsedContacts) {
-      this.setState({contacts: parsedContacts})
-    }
-  }
-// При обновлении приложения стадия Обновления
-  componentDidUpdate(prevProps, prevState) {
-    console.log(prevState)
-    console.log(this.state)
-    if (this.state.contacts !== prevState.contacts) {
-      console.log('Обновились contacts')
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.page !== this.state.page ||
+      prevState.value !== this.state.value
+    ) {
+      this.findImages();
     }
   }
 
-  addContact = ({ name, number }) => {
-    const newContact = {
-      id: nanoid(),
-      name,
-      number,
-    };
+  addValue = ({ inputValue }) => {
+    if (inputValue !== this.state.value) {
+      this.setState({
+        value: inputValue,
+        images: [],
+        page: 1,
+      });
+    } else {
+      this.setState({
+        value: inputValue,
+      });
+    }
+  };
 
-    const { contacts } = this.state;
+  loadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
 
-    contacts.find(
-      contact => newContact.name.toLowerCase() === contact.name.toLowerCase()
-    )
-      ? alert(`${newContact.name} is already in contacts.`)
-      : this.setState(({ contacts }) => ({
-          contacts: [newContact, ...contacts],
+  findImages = async () => {
+    try {
+      this.setState({ isLoading: true });
+      const photos = await getImages(this.state.value, this.state.page);
+      photos.hits.length === 0
+        ? Notify.failure(
+          'Sorry! There is no photo with this name. Try something else!'
+        )
+        : this.setState(prevState => ({
+          images: [...prevState.images, ...photos.hits],
         }));
-  };
-
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  filterInputHandler = event => {
-    this.setState({
-      filter: event.currentTarget.value,
-    });
-  };
-
-  filterContactsOnChange = () => {
-    const { contacts, filter } = this.state;
-    const capitalFilter = filter.toUpperCase();
-    return contacts.filter(contact =>
-      contact.name.toUpperCase().includes(capitalFilter)
-    );
-  };
-
-  sortContactList = () => {
-    return this.filterContactsOnChange().sort(
-      (firstContactName, secondContactName) =>
-        firstContactName.name.localeCompare(secondContactName.name)
-    );
-  };
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
 
   render() {
-    const filteredList = this.sortContactList();
-
+    const { images, isLoading } = this.state;
     return (
-      <>
-        <section >
-          <Container >
-            <h1>Phonebook</h1>
-            <ContactForm onSubmit={this.addContact} />
-            <h2>Contacts</h2>
-            <Filter
-              value={this.state.filter}
-              onChange={this.filterInputHandler}
-            />
-            <ContactList
-              contacts={filteredList}
-              deleteOnClick={this.deleteContact}
-            />
-          </Container>
-          </section>
-      </>
+      <AppBox>
+        <Global styles={GlobalStyles} />
+        <Searchbar onSubmit={this.addValue} />
+        {isLoading && images.length === 0 ? (
+          <Loader />
+        ) : (
+          <ImageGallery items={images} />
+        )}
+        {images.length % 2 === 0 && images.length !== 0 ? (
+          <Button onClick={this.loadMore} />
+        ) : (
+          ''
+        )}
+      </AppBox>
     );
   }
 }
-
+  
 export default App;
-
-
-
-
